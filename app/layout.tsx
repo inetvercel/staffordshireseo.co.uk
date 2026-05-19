b2159db994ecbcb773628096684143d8f3ed9da4
inetvercel <gamerhomenetwork@gmail.com>
2026-05-19 12:11:27 +0100

    Add GA4 tracking (G-PD02DQFDEM) to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -141,6 +141,20 @@ export default function RootLayout({
     <html lang="en-GB" className={`${inter.variable} ${spaceGrotesk.variable}`}>
       <head>
         <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-PD02DQFDEM"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PD02DQFDEM');
+            `,
+          }}
+        />
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify(localBusinessSchema),
